fd48dbca68b78809432acc02f5a788ce70b1a094
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-01-02 03:11:10 +0900

    Update Insertion-Sort-Advanced-Analysis.cpp

--- Insertion-Sort-Advanced-Analysis.cpp
+++ Insertion-Sort-Advanced-Analysis.cpp
@@ -1,3 +1,23 @@
+template <typename T>
+void insertion_sort(T& arr)
+{
+    int n = arr.size();
+
+    for (int i = 1; i < n; ++i)
+    {
+        int key = arr[i];
+        int j;
+
+        for (j = i-1; j >= 0 && arr[j] > key; --j)
+        {
+            arr[j+1] = arr[j];
+        }
+
+        arr[j+1] = key;
+    }
+}
+
+
 int insertionSort(vector<int> arr)
 {
     int result = 0;
