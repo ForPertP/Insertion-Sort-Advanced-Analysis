7576a9bee9e5c3181dfc2908d6d8e5f1dd8064e4
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-01-05 12:58:23 +0900

    Update Insertion-Sort-Advanced-Analysis.cpp

--- Insertion-Sort-Advanced-Analysis.cpp
+++ Insertion-Sort-Advanced-Analysis.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'insertionSort' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+ 
+long int g_count = 0;
+ 
 template <typename T>
 void merge(T& arr, int begin, int mid, int end)
 {
@@ -29,11 +46,13 @@ void merge(T& arr, int begin, int mid, int end)
         {
             arr[k] = R[j];
             j++;
+            
+            g_count += nL - i;
         }
+        
         k++;
     }
 
-    // Copy the remaining elements of L[]
     while (i < nL)
     {
         arr[k] = L[i];
@@ -60,26 +79,91 @@ void merge_sort(T& arr, int begin, int end)
         merge(arr, begin, mid, end);
     }
 }
+ 
 
-int insertionSort(vector<int> arr)
+long int insertionSort(vector<int> arr)
 {
-    int count = 0;
+    g_count = 0;
     
-    int n = arr.size();
+    int index = std::distance(arr.begin(), arr.end());
+    merge_sort(arr, 0, index - 1);
+    
+    return g_count;
+}
 
-    for (int i = 1; i < n; ++i)
-    {
-        int key = arr[i];
-        int j;
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
 
-        for (j = i-1; j >= 0 && arr[j] > key; --j)
-        {
-            arr[j+1] = arr[j];
-            count++;
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string arr_temp_temp;
+        getline(cin, arr_temp_temp);
+
+        vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++) {
+            int arr_item = stoi(arr_temp[i]);
+
+            arr[i] = arr_item;
         }
 
-        arr[j+1] = key;
+        long int result = insertionSort(arr);
+
+        fout << result << "\n";
     }
-    
-    return count;
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
 }
