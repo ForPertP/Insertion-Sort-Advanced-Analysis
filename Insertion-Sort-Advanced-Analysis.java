31cd1ffece6c330ef02bef32d7fc124ba829a617
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-07-12 16:44:43 +0900

    Update Insertion-Sort-Advanced-Analysis.java

--- Insertion-Sort-Advanced-Analysis.java
+++ Insertion-Sort-Advanced-Analysis.java
@@ -9,3 +9,14 @@ import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+
+
+class Result {
+
+    /*
+     * Complete the 'insertionSort' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+}
