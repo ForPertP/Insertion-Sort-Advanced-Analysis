68d461816956ab011436f247def8f965f672f613
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-07-17 07:25:42 +0900

    Update Insertion-Sort-Advanced-Analysis2.java

--- Insertion-Sort-Advanced-Analysis2.java
+++ Insertion-Sort-Advanced-Analysis2.java
@@ -21,3 +21,33 @@ class Result {
      */
 
 }
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                long result = Result.insertionSort(arr);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
